8c20428a594c22af898366dfdc99aa644d24cb35
Gavin Nishizawa <ghn6069@rit.edu>
2016-11-05 15:35:04 -0400

    Add Risk Test file

---
+++ test/junit/net/yura/domination/engine/RiskTest.java
@@ -0,0 +1,17 @@
+package net.yura.domination.engine;
+
+import junit.framework.TestCase;
+
+public class RiskTest extends TestCase {
+    
+    public RiskTest(String testName) {
+        super(testName);
+    }
+
+    public void testWhoWon() {
+        System.out.println("whoWon");
+        
+        assertEquals(true,true);
+    }
+    
+}
