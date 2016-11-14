f78ad6c61dce11c3b5429e02a4637e39d9067121
James C. De Ricco <jcd7042@rit.edu>
2016-11-14 16:13:45 -0500

    Disable a test that forces the tester to find luca.map.

--- test/junit/net/yura/domination/engine/ai/AISimulationTest.java
+++ test/junit/net/yura/domination/engine/ai/AISimulationTest.java
@@ -20,6 +20,7 @@ public class AISimulationTest extends TestCase{
 	int other;
 	
 	public void testSomething() throws InterruptedException {
+                fail("testSomething waits for the user to locate luca.map and crashes when not selected");
 		RiskUIUtil.parseArgs(new String[] {});
 		final Risk risk = new Risk();
 		risk.addRiskListener(new RiskAdapter() {
